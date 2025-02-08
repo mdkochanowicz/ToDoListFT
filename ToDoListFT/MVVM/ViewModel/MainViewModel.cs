cc95ab6ae4c7d8aa5dd0e73fde3d443529233556
Michał Kochanowicz <michal.kochanowicz@student.pk.edu.pl>
2025-02-08 01:06:34 +0100

    Tasks section

--- ToDoListFT/MVVM/ViewModel/MainViewModel.cs
+++ ToDoListFT/MVVM/ViewModel/MainViewModel.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoListFT.MVVM.Model;
 
 namespace ToDoListFT.MVVM.ViewModel
 {
     public class MainViewModel
     {
+        public ObservableCollection<MyTask> Tasks { get; set; }
+        
+        public MainViewModel()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            Tasks = new ObservableCollection<MyTask>
+            {
+                new MyTask
+                {   Title = "Task 1",
+                    Description = "Description 1",
+                    IsCompleted = false
+                },
+                new MyTask
+                {   Title = "Task 2",
+                    Description = "Description 2",
+                    IsCompleted = false
+                },
+                new MyTask
+                {   Title = "Task 3",
+                    Description = "Description 3",
+                    IsCompleted = false
+                }
+            };
+        }
+
     }
 }
